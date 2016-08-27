eed24dff4f724d626fc4dd3fb8fe11289861ccce
Adam Cooke <me@adamcooke.io>
2016-08-27 13:55:22 +0100

    add a callback_url method to fix the breaking change introduced by intridea/omniauth-oauth2@2615267

--- lib/omniauth/strategies/atech.rb
+++ lib/omniauth/strategies/atech.rb
@@ -3,16 +3,16 @@ require 'omniauth-oauth2'
 module OmniAuth
   module Strategies
     class Atech < OmniAuth::Strategies::OAuth2
-      
+
       # Set the site
       option :client_options, {:site => 'https://identity.atechmedia.com'}
-      
+
       # Always request the 'email_address' when authenticating using Omniauth.
       option :authorize_params, {:scope => 'email_address'}
 
-      # Set the UID 
+      # Set the UID
       uid { raw_info['identifier'].to_s }
-      
+
       # Set the all the default info properties as best we can
       info do
         {
@@ -24,12 +24,17 @@ module OmniAuth
           'last_name' => raw_info['name']['last'],
         }
       end
-      
+
       # Provide access to the user's raw info
       extra do
          {'raw_info' => raw_info}
       end
-      
+
+      # Set the callback URL
+      def callback_url
+        full_host + script_name + callback_path
+      end
+
       # Return all the raw information for the user
       def raw_info
         @raw_info ||= begin
@@ -38,7 +43,7 @@ module OmniAuth
            access_token.get('oauth/api/profile').parsed
         end
       end
-      
+
       # Return the user's first e-mail address
       def email_address
          raw_info['email_addresses'] && raw_info['email_addresses'].first
