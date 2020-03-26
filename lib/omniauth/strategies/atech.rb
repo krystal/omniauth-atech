53bb4a3a921ad68256c7a2a797d59cd2d33bccbd
Georgi Ganchev <googs.bs@gmail.com>
2020-03-26 09:44:36 +0000

    adds missing loader

--- lib/omniauth/strategies/atech.rb
+++ lib/omniauth/strategies/atech.rb
@@ -5,10 +5,10 @@ module OmniAuth
     class Atech < OmniAuth::Strategies::OAuth2
 
       # Set the site
-      option :client_options, {:site => 'https://identity.atechmedia.com'}
+      option :client_options, { :site => 'https://identity.atechmedia.com' }
 
       # Always request the 'email_address' when authenticating using Omniauth.
-      option :authorize_params, {:scope => 'email_address'}
+      option :authorize_params, { :scope => 'email_address' }
       option :authorize_options, [:prompt]
 
       # Set the UID
@@ -28,7 +28,7 @@ module OmniAuth
 
       # Provide access to the user's raw info
       extra do
-         {'raw_info' => raw_info}
+        { 'raw_info' => raw_info }
       end
 
       # Set the callback URL
@@ -39,15 +39,15 @@ module OmniAuth
       # Return all the raw information for the user
       def raw_info
         @raw_info ||= begin
-           access_token.options[:mode] = :query
-           access_token.options[:param_name] = 'oauth_token'
-           access_token.get('oauth/api/profile').parsed
+          access_token.options[:mode] = :query
+          access_token.options[:param_name] = 'oauth_token'
+          access_token.get('oauth/api/profile').parsed
         end
       end
 
       # Return the user's first e-mail address
       def email_address
-         raw_info['email_addresses'] && raw_info['email_addresses'].first
+        raw_info['email_addresses'] && raw_info['email_addresses'].first
       end
 
     end
